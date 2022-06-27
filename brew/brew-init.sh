4e8cc439d536a859c41c21edc596a4d3489f6faf
Allen Chen <allen.chen@autodesk.com>
2022-06-27 22:40:02 +0800

    -Add wangwang

--- brew/brew-init.sh
+++ brew/brew-init.sh
@@ -9,9 +9,9 @@ else
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
-brew install python3 virtualenv macvim node@16 npm yarn awscli vault fzf rg ag jq gopls
+brew install python3 virtualenv macvim node@16 npm yarn awscli vault fzf rg ag jq gopls ctags
 
-brew install --cask postman visual-studio-code gimp docker microsoft-remote-desktop nextcloud evernote firefox microsoft-edge google-chrome
+brew install --cask postman visual-studio-code gimp docker microsoft-remote-desktop nextcloud evernote firefox microsoft-edge google-chrome whatsapp
 
 # install language servers
 npm install -g typescript-language-server dockerfile-language-server-nodejs bash-language-server vscode-css-languageserver-bin prettier eslint
